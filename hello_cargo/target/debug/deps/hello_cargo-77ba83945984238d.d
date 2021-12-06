b138a98edba4444ed71f0422744910c53c4c52b2
V-Goo <v-go@ya.ru>
2021-12-06 14:52:46 +0300

    start whith Cargo

---
+++ hello_cargo/target/debug/deps/hello_cargo-77ba83945984238d.d
@@ -0,0 +1,5 @@
+/home/maccar/2-training/rust-training/hello_cargo/target/debug/deps/hello_cargo-77ba83945984238d: src/main.rs
+
+/home/maccar/2-training/rust-training/hello_cargo/target/debug/deps/hello_cargo-77ba83945984238d.d: src/main.rs
+
+src/main.rs:
---
+++ hello_cargo/target/debug/hello_cargo.d
@@ -0,0 +1,1 @@
+/home/maccar/2-training/rust-training/hello_cargo/target/debug/hello_cargo: /home/maccar/2-training/rust-training/hello_cargo/src/main.rs
