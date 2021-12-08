d7f552b6d9dc2b8e2ce916aaa071966baa385828
V-Goo <v-go@ya.ru>
2021-12-08 18:06:08 +0300

    initial commit ex. branches

---
+++ branches/target/rls/debug/deps/branches-912a6e07506c5706.d
@@ -0,0 +1,5 @@
+/home/maccar/2-training/rust-training/branches/target/rls/debug/deps/branches-912a6e07506c5706.rmeta: src/main.rs
+
+/home/maccar/2-training/rust-training/branches/target/rls/debug/deps/branches-912a6e07506c5706.d: src/main.rs
+
+src/main.rs:
