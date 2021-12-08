43e1cf61215e66fd4b9b142f085243e2a8b8b4ad
V-Goo <v-go@ya.ru>
2021-12-08 18:18:34 +0300

    initial commit ex. loops

---
+++ loops/target/rls/debug/deps/loops-8bd76c09b3c6b61a.d
@@ -0,0 +1,5 @@
+/home/maccar/2-training/rust-training/loops/target/rls/debug/deps/loops-8bd76c09b3c6b61a.rmeta: src/main.rs
+
+/home/maccar/2-training/rust-training/loops/target/rls/debug/deps/loops-8bd76c09b3c6b61a.d: src/main.rs
+
+src/main.rs:
---
+++ loops/target/rls/debug/deps/loops-c81a00915507dcec.d
@@ -0,0 +1,5 @@
+/home/maccar/2-training/rust-training/loops/target/rls/debug/deps/loops-c81a00915507dcec.rmeta: src/main.rs
+
+/home/maccar/2-training/rust-training/loops/target/rls/debug/deps/loops-c81a00915507dcec.d: src/main.rs
+
+src/main.rs:
