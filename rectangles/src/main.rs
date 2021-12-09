0c87e102f0d6ca830d34519315caa38f46d78444
V-Goo <v-go@ya.ru>
2021-12-09 19:28:50 +0300

    rectangles plus struct and pretty out and method

--- rectangles/src/main.rs
+++ rectangles/src/main.rs
@@ -4,6 +4,16 @@ struct Rectangle {
     height: u32,
 }
 
+impl Rectangle {
+    fn area(&self) -> u32{
+        self.width * self.height
+    }
+
+    fn can_hold(&self, other: &Rectangle) -> bool {
+        self.width > other.width && self.height > other.height
+    }
+}
+
 fn main() {
     let width1 = 30;
     let height1 = 50;
@@ -24,12 +34,34 @@ fn main() {
         height: 50,
     };
 
+    let rect_struct_1 = Rectangle {
+        width: 30,
+        height: 50,
+    };
+    let rect_struct_2 = Rectangle {
+        width: 10,
+        height: 40,
+    };
+    let rect_struct_3 = Rectangle {
+        width: 60,
+        height: 45,
+    };
+
+    println!("Can rect1 hold rect2? {}", rect_struct_1.can_hold(&rect_struct_2));
+    println!("Can rect1 hold rect3? {}", rect_struct_1.can_hold(&rect_struct_3));
+    
     println!("\nrect_struct is {:?}\n", rect_struct);
     println!("rect_struct is {:#?}", rect_struct);
 
     println!(
-        "The area of the rectangle is {} square pixels.",
+        "The area of the rectangle_struct is {} square pixels.",
         area_struct(&rect_struct)
+    );
+
+    
+    println!(
+        "\nThe area  of the rectangle is {} square pixels.",
+        rect_struct.area()
     );
 }
 
