db0fd923d56154a0f150e7df7658683088f06ede
V-Goo <v-go@ya.ru>
2021-12-09 18:45:40 +0300

    rectangles tuple helper

--- rectangles/src/main.rs
+++ rectangles/src/main.rs
@@ -1,3 +1,23 @@
 fn main() {
-    println!("Hello, world!");
+    let width1 = 30;
+    let height1 = 50;
+
+    println!(
+        "The area of the rectangle is {} square pixels.",
+        area(width1, height1)
+    );
+    let rect1 = (30, 50);
+
+    println!(
+        "The area of the rectangle is {} square pixels.",
+        area1(rect1)
+    );
+}
+
+fn area1(dimensions: (u32, u32)) -> u32 {
+    dimensions.0 * dimensions.1
+}
+
+fn area(width: u32, height: u32) -> u32 {
+    width * height
 }
---
+++ rectangles/target/rls/debug/deps/rectangles-6d24ea3a487198dd.d
@@ -0,0 +1,5 @@
+/home/maccar/2-training/rust-training/rectangles/target/rls/debug/deps/rectangles-6d24ea3a487198dd.rmeta: src/main.rs
+
+/home/maccar/2-training/rust-training/rectangles/target/rls/debug/deps/rectangles-6d24ea3a487198dd.d: src/main.rs
+
+src/main.rs:
