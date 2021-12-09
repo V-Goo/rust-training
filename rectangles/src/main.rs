a8d00beb8c3043baef3517cd53755955a9554c3f
V-Goo <v-go@ya.ru>
2021-12-09 18:51:05 +0300

    rectangles plus struct

--- rectangles/src/main.rs
+++ rectangles/src/main.rs
@@ -1,3 +1,8 @@
+struct Rectangle {
+    width: u32,
+    height: u32,
+}
+
 fn main() {
     let width1 = 30;
     let height1 = 50;
@@ -12,6 +17,20 @@ fn main() {
         "The area of the rectangle is {} square pixels.",
         area1(rect1)
     );
+
+    let rect_struct = Rectangle {
+        width: 30,
+        height: 50,
+    };
+
+    println!(
+        "The area of the rectangle is {} square pixels.",
+        area_struct(&rect_struct)
+    );
+}
+
+fn area_struct(rectangle: &Rectangle) -> u32 {
+    rectangle.width * rectangle.height
 }
 
 fn area1(dimensions: (u32, u32)) -> u32 {
