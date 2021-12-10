587f85da7c2f370cc27656834dd2df13ccc0210c
V-Goo <v-go@ya.ru>
2021-12-10 14:19:29 +0300

    enum and function

---
+++ coin_rust/target/rls/debug/deps/coin_rust-475f66c5034f7aa1.d
@@ -0,0 +1,5 @@
+/home/maccar/2-training/rust-training/coin_rust/target/rls/debug/deps/coin_rust-475f66c5034f7aa1.rmeta: src/main.rs
+
+/home/maccar/2-training/rust-training/coin_rust/target/rls/debug/deps/coin_rust-475f66c5034f7aa1.d: src/main.rs
+
+src/main.rs:
