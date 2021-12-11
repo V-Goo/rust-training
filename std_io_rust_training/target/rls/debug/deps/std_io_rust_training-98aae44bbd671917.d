00b7c0dd331b673f9882bcaf0ef00c343125fc43
V-Goo <v-go@ya.ru>
2021-12-11 23:56:20 +0300

    std_io_rust_training initial commit true

---
+++ std_io_rust_training/target/rls/debug/deps/std_io_rust_training-98aae44bbd671917.d
@@ -0,0 +1,5 @@
+/home/maccar/2-training/rust-training/std_io_rust_training/target/rls/debug/deps/std_io_rust_training-98aae44bbd671917.rmeta: src/main.rs
+
+/home/maccar/2-training/rust-training/std_io_rust_training/target/rls/debug/deps/std_io_rust_training-98aae44bbd671917.d: src/main.rs
+
+src/main.rs:
