41340183aa0893a78d15a3c53d29a75b3b798963
V-Goo <v-go@ya.ru>
2021-12-13 15:52:08 +0300

    delete from the index target/* file

--- coin_rust/target/rls/debug/deps/coin_rust-41ba3e024db9c05e.d
+++
@@ -1,5 +0,0 @@
-/home/maccar/2-training/rust-training/coin_rust/target/rls/debug/deps/coin_rust-41ba3e024db9c05e.rmeta: src/main.rs
-
-/home/maccar/2-training/rust-training/coin_rust/target/rls/debug/deps/coin_rust-41ba3e024db9c05e.d: src/main.rs
-
-src/main.rs:
