99d282060a28c161021c758dc334129f9c0540f6
V-Goo <v-go@ya.ru>
2021-12-16 02:35:21 +0300

    ref: add panic in input

--- minigrep/src/main.rs
+++ minigrep/src/main.rs
@@ -4,7 +4,7 @@ use std::fs;
 fn main() {
     let args: Vec<String> = env::args().collect();
 
-    let config = parse_config(&args);
+    let config = Config::new(&args);
 
     println!("Searching for {}", config.query);
     println!("In file {}", config.filename);
@@ -18,10 +18,14 @@ struct Config {
     query: String,
     filename: String,
 }
+impl Config {
+    fn new(args: &[String]) -> Config {
+        if args.len() < 3 {
+            panic!("Малавато будет")
+        }
+        let query = args[1].clone();
+        let filename = args[2].clone();
 
-fn parse_config(args: &[String]) -> Config {
-    let query = args[1].clone();
-    let filename = args[2].clone();
-
-    Config { query, filename }
+        Config { query, filename }
+    }
 }
