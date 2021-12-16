510832e57107a8ed75b8d615c66033a259e5d11b
V-Goo <v-go@ya.ru>
2021-12-16 13:50:26 +0300

    feat: test fn search pass

--- minigrep/src/lib.rs
+++ minigrep/src/lib.rs
@@ -1,24 +1,51 @@
 use std::error::Error;
 use std::fs;
 
-pub fn run(config: Config) -> Result<(), Box<dyn Error>>{
-	let contetnt = fs::read_to_string(config.filename)?;
-	println!("With text:\n{}", contetnt);
-	Ok(())
+pub fn run(config: Config) -> Result<(), Box<dyn Error>> {
+    let contetnt = fs::read_to_string(config.filename)?;
+    println!("With text:\n{}", contetnt);
+    Ok(())
 }
 
 pub struct Config {
-	pub query: String,
-	pub filename: String,
+    pub query: String,
+    pub filename: String,
 }
-impl Config {
-	pub fn new(args: &[String]) -> Result<Config, &str> {
-			if args.len() < 3 {
-					return Err("Малавато будет");
-			}
-			let query = args[1].clone();
-			let filename = args[2].clone();
 
-			Ok(Config { query, filename })
+impl Config {
+    pub fn new(args: &[String]) -> Result<Config, &str> {
+        if args.len() < 3 {
+            return Err("Малавато будет");
+        }
+        let query = args[1].clone();
+        let filename = args[2].clone();
+
+        Ok(Config { query, filename })
+    }
+}
+
+#[cfg(test)]
+mod tests {
+    use super::*;
+
+    #[test]
+    fn one_result() {
+        let query = "duc";
+        let contetnt = "\
+Rust:
+safe, fast, productive.
+Pick three.";
+
+        assert_eq!(vec!["safe, fast, productive."], search(query, contetnt));
+    }
+}
+// ! мы говорим Rust, что данные, возвращаемые функцией search, будут жить до тех пор, пока живут данные, переданные в функцию search через аргумент contents
+pub fn search<'a>(query: &str, contetnt: &'a str) -> Vec<&'a str> {
+	let mut results = Vec::new();
+	for line in contetnt.lines() {
+		if line.contains(query) {
+			results.push(line);
+		}
 	}
+	results
 }
