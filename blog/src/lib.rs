ef08f9a42baae3ae95141edd0e58f6d90f370533
V-Goo <v-go@ya.ru>
2021-12-21 15:33:31 +0300

    blog State finish

--- blog/src/lib.rs
+++ blog/src/lib.rs
@@ -12,8 +12,11 @@ impl Post {
     pub fn add_text(&mut self, text: &str) {
         self.content.push_str(text);
     }
+    // pub fn content(&self) -> &str {
+    //     ""
+    // }
     pub fn content(&self) -> &str {
-        ""
+        self.state.as_ref().unwrap().content(self)
     }
     pub fn request_review(&mut self) {
         if let Some(s) = self.state.take() {
@@ -31,6 +34,9 @@ impl Post {
 trait State {
     fn request_review(self: Box<Self>) -> Box<dyn State>;
     fn approve(self: Box<Self>) -> Box<dyn State>;
+    fn content<'a>(&self, post: &'a Post) -> &'a str {
+        ""
+    }
 }
 
 struct Draft {}
@@ -61,5 +67,8 @@ impl State for Published {
 
     fn approve(self: Box<Self>) -> Box<dyn State> {
         self
+    }
+    fn content<'a>(&self, post: &'a Post) -> &'a str {
+        &post.content()
     }
 }
