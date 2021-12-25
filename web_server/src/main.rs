2f54d6968d85bb23a0f9940d203a9a70855917b3
V-Goo <v-go@ya.ru>
2021-12-25 23:18:48 +0300

    fix: allow unused variables

--- web_server/src/main.rs
+++ web_server/src/main.rs
@@ -2,7 +2,7 @@ use std::net::TcpListener;
 
 fn main() {
     let listener = TcpListener::bind("127.0.01:7878").unwrap();
-
+#[allow(unused_variables)]
     for stream in listener.incoming() {
         let stream = stream.unwrap();
         println!("Присоединяйся!");
