7622fe5fc0a85de6d45a6c6f53dee9f42f5b27f2
V-Goo <v-go@ya.ru>
2021-12-31 20:09:28 +0300

    impl struct Worker and new() method

--- web_server/src/lib.rs
+++ web_server/src/lib.rs
@@ -1,8 +1,10 @@
 use std::thread;
  
 pub struct ThreadPool{
-	threads: Vec<thread::JoinHandle<()>>,
+	workers: Vec<Worker>,
 }
+
+
 
 
 impl ThreadPool {
@@ -15,11 +17,11 @@ impl ThreadPool {
 	/// The `new` function will panic if the size is zero.
 	pub fn new(size: usize) -> ThreadPool {
 		assert!(size > 0);
-		let mut threads = Vec::with_capacity(size);
-		for _ in 0..size {
-			// 
+		let mut workers = Vec::with_capacity(size);
+		for id  in 0..size {
+			workers.push(Worker::new(id));
 		}
-		ThreadPool { threads }
+		ThreadPool { workers }
 	}
 
 	pub fn execute<F>(&self, f: F)
@@ -28,4 +30,17 @@ impl ThreadPool {
 			{
 
 			}
+}
+
+struct Worker {
+	id: usize,
+	thread: thread::JoinHandle<()>,
+}
+
+impl Worker {
+	fn new(id: usize) -> Worker {
+		let thread = thread::spawn(|| {});
+
+		Worker {id, thread}
+	}
 }
